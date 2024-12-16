503bda523f8e25a24eacfea9a602c0cacc256e53
rakibultron <mdrakibul.dev@gmail.com>
2024-12-17 05:06:26 +0600

    feat: add separate authentication, main, and admin layouts

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -1,11 +1,41 @@
-import Home from "@/pages/home/Home";
 import { useRoutes } from "react-router-dom";
+
+import AuthLayout from "@/layouts/AuthLayout";
+import Login from "@/pages/auth/login/Login";
+import Home from "@/pages/home/Home";
+import MainLayout from "@/layouts/MainLayout";
+import About from "@/pages/about/About";
+import Register from "@/pages/auth/register/Register";
 
 const AppRouter = () => {
   return useRoutes([
     {
       path: "/",
-      element: <Home />,
+      element: <MainLayout />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+      ],
+    },
+    {
+      path: "/auth",
+      element: <AuthLayout />,
+      children: [
+        {
+          path: "login",
+          element: <Login />,
+        },
+        {
+          path: "register",
+          element: <Register />,
+        },
+      ],
     },
   ]);
 };
