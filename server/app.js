ebf1012307fe696cb0fd6775f374850f942d6ef6
rakibultron <mdrakibul.dev@gmail.com>
2024-12-17 05:58:33 +0600

    feat: add cors configured

--- server/app.js
+++ server/app.js
@@ -1,13 +1,29 @@
-require("dotenv").config();
+const routes = require("./routers/routes");
 const express = require("express");
 const morgan = require("morgan");
-const routes = require("./routers/routes");
+const cors = require("cors")
+require("dotenv").config();
 const app = express();
+
 
 // Middleware setup
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// Cors
+app.use(
+    cors({
+        origin: [
+            "https://arnifi-blog.vercel.app"
+        ],
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        credentials: true,
+    })
+);
 
 
 // API Routes
