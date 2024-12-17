6b68ed2f9b63dc2111ad213e2a67b41eb87ae0f8
rakibul-dev <mdrakibul.dev@gmail.com>
2024-12-17 19:37:32 +0600

    feat: add delete blog by author route

--- server/routers/v1/blogRoute.js
+++ server/routers/v1/blogRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createBlog, getAllBlogs } = require('../../controllers/blogControlers')
+const { createBlog, getAllBlogs, deleteBlogHandler } = require('../../controllers/blogControlers')
 const authenticate = require('../../middlewares/authMiddleware')
 
 
 router.post('/', authenticate, createBlog)
 router.get('/', authenticate, getAllBlogs)
+router.delete('/:id', authenticate, deleteBlogHandler);
 
 
 module.exports = router;
