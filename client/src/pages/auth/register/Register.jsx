b1052f1eae15df65f02d9e04d6a289b673205366
rakibultron <mdrakibul.dev@gmail.com>
2024-12-18 00:21:38 +0600

    refactor: add register page ui improvement

--- client/src/pages/auth/register/Register.jsx
+++ client/src/pages/auth/register/Register.jsx
@@ -1,5 +1,21 @@
+// import RegisterForm from "./components/RegisterForm";
+
+// const Register = () => {
+//   return <RegisterForm />;
+// };
+
+// export default Register;
+
+import RegisterForm from "./components/RegisterForm";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="mx-auto max-w-sm w-full">
+        <RegisterForm />
+      </div>
+    </div>
+  );
 };
 
 export default Register;
